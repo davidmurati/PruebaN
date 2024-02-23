65209602a3c2b65566f4bc1e9197b62f7727f5d4
David Murati <davidmurati1@gmail.com>
2024-02-23 15:22:27 -0400

    24 commit neurofy

--- src/Component/Efectos/EfectoEscritura.jsx
+++ src/Component/Efectos/EfectoEscritura.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 function EfectoEscritura({ text, mLeftInicial = 180, transitionTime = 2, randCar, tickCambioLetra = 150 }) {
     
     const [marginLeft, setMarginLeft] = useState(mLeftInicial ? mLeftInicial : 0)
-    const caracteresRandom = randCar ? randCar : ['❕', '💭', '🗯', '🗨', '💬', '💭', '🗯', '🗨', '❔', '💭', '❔', '🖊', '🖊']
+    const caracteresRandom = randCar ? randCar : ['#', '#', '#', '#', '💬', '💭', '🗯', '🗨', '❔', '#', '#', '#', '#']
     const [newText, setNewText] = useState('')
     // Para el randomIntFromInterval
     const min = 0
